f6f0f68a76f67875af77bd221a9846a16efc67c1
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-04-27 15:03:44 +0500

    Added dept, subjects and semester models

--- uettAssistant/uettApp/models.py
+++ uettAssistant/uettApp/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nickname
+    
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    associated_name = models.CharField(max_length=200, default="not_assigned")  # Change default value
+    credit_hour = models.IntegerField(default=3)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE,default=1)  # Add this line
+
+    def __str__(self):
+        return self.associated_name
+    
+    def save(self, *args, **kwargs):
+        if self.associated_name == 'not_assigned':  # If associated_name is not provided, set it to the associated department's nickname
+            self.associated_name = f'{self.name} - {self.department.nickname}'
+        super().save(*args, **kwargs)
+
+
+
+# class Subject(models.Model):
+#     name = models.CharField(max_length=200)
+#     associated_name = models.CharField(max_length=200, default="not_assigned")  # Change default value
+#     credit_hour = models.IntegerField(default=3)
+
+#     def __str__(self):
+#         return self.name
+    
+# class Semester(models.Model):
+#     name = models.CharField(max_length=200)
+#     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+#     associated_name = models.CharField(max_length=200,default=department.nickname)
+#     subjects = models.ManyToManyField(Subject)
+
+#     def __str__(self):
+#         return self.associated_name
+
+class Semester(models.Model):
+    name = models.CharField(max_length=200)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    associated_name = models.CharField(max_length=200, default="not_assigned")  # Change default value
+    subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.associated_name
+
+    def save(self, *args, **kwargs):
+        if self.associated_name == 'not_assigned':  # If associated_name is not provided, set it to department's nickname
+            self.associated_name = f'{self.name} - {self.department.nickname}'
+        super().save(*args, **kwargs)
--- uettAssistant/uettApp/views.py
+++ uettAssistant/uettApp/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+# def home(request):
+#     deps = Department.objects.all()
+#     context = {'deps':deps}
+#     return render(request,'uettApp/home.html',context)
+
+
+def home(request):
+    departments = Department.objects.all()
+    context = {'departments': departments}
+    return render(request, 'uettApp/home.html', context)
+
+
+# def home(request):
+#     # Retrieve all departments
+#     departments = Department.objects.all()
+    
+#     # Create an empty dictionary to store departments and their associated semesters
+#     department_semesters = {}
+
+#     # Loop through each department
+#     for department in departments:
+#         # Retrieve all semesters related to the current department
+#         semesters = department.semester_set.all()
+#         # Store the department and its semesters in the dictionary
+#         department_semesters[department] = semesters
+
+#     # Pass the dictionary to the template context
+#     context = {'department_semesters': department_semesters}
+#     return render(request, 'uettApp/home.html', context)
